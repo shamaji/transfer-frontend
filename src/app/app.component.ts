81128071fda862940070fd61577b6ac208ce927a
CHAVADA SHAMAJI <chavadashamaji@CHAVADAs-MacBook-Air.local>
2020-01-26 14:53:19 +0530

    added dashboard data displayed and user will get dashboard only if he login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UtilsService } from './Service/UtilService.service';
+import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'frontend';
   options = { position: ['top', 'center'] };
+  constructor(public utilsService: UtilsService, public router: Router, private route: ActivatedRoute) {
+    router.events.forEach(val => {
+      if (val instanceof NavigationStart) {
+        console.log(' Current Url : ' + val.url);
+        if (this.utilsService.getUserDetails() == null) {
+          if (val.url !== '/' && val.url !== '/sign-up' && val.url !== '/sign-up/sign-in') {
+            console.log('go to login');
+            this.utilsService.redirectTo('/sign-up/sign-in');
+          } else { console.log('already on login page'); }
+        } else {
+          console.log('iddddddd')
+        }
+      }
+    });
+
+  }
 }
